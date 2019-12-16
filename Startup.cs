6d7b13e8d6308054bced79e6a40e614c89433457
Gaurav Agerwala <gauravagerwala@gmail.com>
2019-12-16 10:43:05 -0600

    Testing pipelines

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace AppServiceDemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure!");
             });
         }
     }
